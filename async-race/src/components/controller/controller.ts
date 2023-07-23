a8863170f4fb92d5f8ff2ba6879d411cf58d415f
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-07-23 21:00:12 +0200

    feat: realize createCar method

--- async-race/src/components/controller/controller.ts
+++ async-race/src/components/controller/controller.ts
@@ -1,26 +1,50 @@
 import Model from '../model/model';
+import STATE from '../model/STATE';
+import { ICar } from '../types/cat.types';
 
 export default class Controller {
     model = new Model();
     readonly path = {
-        garage: '/garage',
-        winners: '/winners'
+        garage: '/garage/',
+        winners: '/winners/'
     };
 
-    async getCars() {
-        return this.model.fetchData(this.path.garage);
+    async getAllCars() {
+        await this.model.getData(this.path.garage)
+            .then((data: ICar[]) => {
+                data.forEach(car => STATE.cars.push(car));
+            })
+            .catch((err) => {
+                alert(`Failed to fetch data. Error message:  ${err.message}. Please, reload the page.`);
+            });
+        console.log(STATE.cars);
+    }
+
+    async getCars(page: number) {
+        return STATE.cars.slice((page - 1) * STATE.carsOnPage, ((page - 1) * STATE.carsOnPage) + STATE.carsOnPage);
     }
 
     getCar() {
 
     }
 
-    createCar() {
+    async createCar(name: string, color: string) {
+        const newCar: object = {
+            name,
+            color
+        };
 
+        const response = this.model.postData('POST',this.path.garage, newCar);
+        await response.then((data) => {
+            STATE.cars.push(data);
+        });
+        console.log(STATE.cars);
+
+        return response;
     }
 
     deleteCar() {
-
+        
     }
 
     updateCar() {
@@ -59,3 +83,20 @@ export default class Controller {
 
     }
 }
+
+ // const queryParams = [
+        //     {
+        //         key: '_page',
+        //         value: page
+        //     },
+        //     {
+        //         key: '_limit',
+        //         value: 5,
+        //     }
+        // ];
+        // queryParams.push();
+
+        // const response = this.model.getData(this.path.garage, queryParams);
+        // await response.then((data: ICar[]) => data.forEach((car) => {
+        //     STATE.cars.push(car);
+        // }));
--- async-race/src/components/view/garage/garage.ts
+++ async-race/src/components/view/garage/garage.ts
@@ -2,26 +2,45 @@ import './garage.css';
 import Controller from '../../controller/controller';
 import createNewElement from "../create-new-element";
 import Tracks from '../tracks/tracks';
+import Pagination from '../pagination/pagination';
 import STATE from '../../model/STATE';
 
 export default class Garage {
     controller = new Controller();
     main: HTMLElement | null = document.querySelector('main');
     tracks = new Tracks();
+    pagination = new Pagination();
 
-    async drawGarage() {
-        const garageSection = createNewElement<HTMLElement>('section', ['section', 'section_garage']);
+    public async drawGarage() {
+        let garageSection = document.querySelector('.section_garage');
+        if (garageSection) {
+            for (let i = 1; i < garageSection.children.length; i++) {
+                const child = garageSection.children[i];
+                console.log(child);
+                garageSection.removeChild(child);
+            }
+            garageSection.innerHTML = '';
+        } else {
+            garageSection = createNewElement<HTMLElement>('section', ['section', 'section_garage']);
+        }
         const garageEditor = this.drawGarageEditor();
+        const garagePagination = this.drawPagination();
         const garageTitle = this.drawGarageTitle();
         const garageTracks = await this.drawTracks();
 
         garageSection.append(garageEditor);
+        garageSection.append(garagePagination);
         garageSection.append(garageTitle);
-        garageSection.append(garageTracks);
+        garageSection.append(garageTracks); 
         return garageSection;
     }
 
-    drawGarageEditor() {
+    async redrawGarage() {
+        this.drawGarage();
+
+    }
+
+    private drawGarageEditor() {
         const garageEditor = createNewElement<HTMLDivElement>('div', ['garage-editor']);
 
         const newCarForm: HTMLFormElement = createNewElement<HTMLFormElement>('div', ['form_add']);
@@ -31,72 +50,39 @@ export default class Garage {
         const newCarColor: HTMLInputElement = createNewElement<HTMLInputElement>('input', ['form__input', 'form__input_color'], {type: 'color'});
         const newCarButton = createNewElement('button', ['form__button_add'], {textContent: 'Add'});
 
+        newCarButton.addEventListener('click', () => {
+            const carsName: string | null = newCarInput.value;
+            const carsColor: string | null = newCarColor.value;
+            if (carsName && carsColor) {
+                const response = this.controller.createCar(carsName, carsColor);
+                response.then(() => this.redrawGarage());
+            } else {
+                alert('Please, name a car!');
+            }
+        });
+
         inputsWrapper.append(newCarInput);
         inputsWrapper.append(newCarColor);
         newCarForm.append(inputsWrapper);
         newCarForm.append(newCarButton);
-
 
         garageEditor.append(newCarForm);
 
         return garageEditor;
     }
 
-    drawGarageTitle() {
+    private drawGarageTitle() {
         const garageTitle = createNewElement<HTMLHeadingElement>('h2', ['section__title'], {textContent: `Garage (${STATE.cars.length})`});
         return garageTitle;
     }
 
-    drawTracks() {
-        const tracksWrapper = this.tracks.drawTracksWrapper();
-
-        return tracksWrapper;
+    private drawPagination() {
+        const pagination = this.pagination.drawPagination();
+        return pagination;
     }
 
-
-
-    // drawPagination() {
-
-    // }
-
-    
-
-
-
-    // async drawGarage() {
-    //     this.main = document.querySelector('main');
-    //     const garageSection = createNewElement<HTMLElement>('section', ['section', 'section_garage']);
-    //     const garageEditor = createNewElement<HTMLDivElement>('div', ['garage-editor']);
-
-    //     const newCarForm: HTMLFormElement = createNewElement<HTMLFormElement>('div', ['form_add']);
-    //     const inputsWrapper = createNewElement('div', ['form__inputs-wrapper']);
-    //     const newCarInput: HTMLInputElement = createNewElement<HTMLInputElement>('input', ['form__input', 'form__input_text'], {placeholder: 'Cars name'});
-    //     const newCarColor: HTMLInputElement = createNewElement<HTMLInputElement>('input', ['form__input', 'form__input_color'], {type: 'color'});
-    //     const newCarButton = createNewElement('button', ['form__button_add'], {textContent: 'Add'});
-
-    //     inputsWrapper.append(newCarInput);
-    //     inputsWrapper.append(newCarColor);
-    //     newCarForm.append(inputsWrapper);
-    //     newCarForm.append(newCarButton);
-
-
-    //     garageEditor.append(newCarForm);
-    //     garageSection.append(garageEditor);
-
-    //     const garageTitle = createNewElement<HTMLHeadingElement>('h2', ['section__title'], {textContent: `Garage (${STATE.cars.length})`});
-    //     garageSection.append(garageTitle);
-    //     this.main?.append(garageSection);
-    //     this.tracks.drawTracksWrapper();
-
-    //     newCarButton.addEventListener('click', () => {
-    //         const carNameValue: string | null = newCarInput.value;
-    //         const carColorValue: string | null = newCarColor.value;
-            
-    //         if (carNameValue && carColorValue) {
-    //             const response = this.controller.createCar(carNameValue, carColorValue);
-    //             response.then(() => this.drawGarage());
-    //         }
-    //     });
-
-    // }
+    private drawTracks() {
+        const tracksWrapper = this.tracks.drawTracksWrapper();
+        return tracksWrapper;
+    }
 }
