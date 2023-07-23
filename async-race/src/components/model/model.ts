7bee87c62c9d981f4f82f1a6422c792491001239
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-07-23 18:58:13 +0200

    feat: realize get and post methods

--- async-race/src/components/model/model.ts
+++ async-race/src/components/model/model.ts
@@ -2,11 +2,39 @@ export default class Model {
     
     readonly baseLink: string = 'http://127.0.0.1:3000';
 
-    async fetchData(endpoint: string) {
-        return await fetch(`${this.baseLink}${endpoint}`)
+    async getData(path: string, queryParams?: Array<IQueryParams>) {
+        const generateQueryString = () => {
+            if (queryParams?.length) {
+                return `?${queryParams.map(param => `${param.key}=${param.value}`).join('&')}`;
+            } else {
+                return '';
+            }
+        };
+
+        let endpoint = `${this.baseLink}${path}`;
+        if (queryParams) {
+            endpoint = `${endpoint}${generateQueryString()}`;
+        }
+        return await fetch(endpoint)
             .then((res) => res.json())
             .then((data) => data);
     }
+
+    async postData(method: string, path: string, data: object) {
+        const endpoint = `${this.baseLink}${path}`;
+        const response = await fetch(endpoint, {
+            method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        const newElement = await response.json();
+        return newElement;
+    }
 }
 
-// type Car = Record<string, string>;
+interface IQueryParams {
+    key: string;
+    value: number;
+}
