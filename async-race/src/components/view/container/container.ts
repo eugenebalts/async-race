7b13c71ab349ee2bbcd80354fe7689f1ac2163ea
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-07-24 19:31:40 +0200

    feat: put page in container

--- async-race/src/components/view/container/container.ts
+++ async-race/src/components/view/container/container.ts
@@ -1,12 +1,27 @@
 import './container.css';
 import createNewElement from "../create-new-element";
+import Main from '../main/main';
+import Header from '../header/header';
 
 export default class Container {
 
     body: HTMLBodyElement | null = document.querySelector('body');
+    main = new Main();
+    header = new Header();
 
-    createContainer() {
+    async drawContainer() {
         const container: HTMLDivElement = createNewElement('div', ['container'], {id: 'container'});
-        this.body?.append(container);
+        const header = this.header.drawHeader();
+        const main = await this.main.drawMain();
+
+        container.append(header);
+        container.append(main);
+
+        return container;
     }
+
+    // createContainer() {
+    //     const container: HTMLDivElement = createNewElement('div', ['container'], {id: 'container'});
+    //     this.body?.append(container);
+    // }
 }
