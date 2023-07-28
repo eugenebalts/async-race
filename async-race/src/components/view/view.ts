9b0601347b3d1cdb34eeed94e312730fc38da345
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-07-28 12:50:07 +0200

    refactor: delete unusable variables and write types in view.ts module

--- async-race/src/components/view/view.ts
+++ async-race/src/components/view/view.ts
@@ -1,17 +1,13 @@
 import './view.css';
 import Container from './container/container';
-import Header from './header/header';
-import Main from './main/main';
 
 export default class View {
-    container = new Container();
-    header = new Header();
-    main = new Main();
-    body = document.querySelector('body');
+    private container = new Container();
+    private body: HTMLBodyElement | null = document.querySelector('body');
 
 
     async render() {
-        const container = await this.container.drawContainer();
+        const container: HTMLDivElement = await this.container.drawContainer();
         this.body?.append(container);
     }
 }
