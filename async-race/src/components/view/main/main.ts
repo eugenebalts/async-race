b6dd5168e4b361d24f616d679b2a0c7a5ee526d6
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-07-29 23:10:39 +0200

    refactor: add types and change quotes in main.ts

--- async-race/src/components/view/main/main.ts
+++ async-race/src/components/view/main/main.ts
@@ -1,32 +1,24 @@
 import './main.css';
-import Navigation from "../navigation/navigation";
-import Garage from "../garage/garage";
+import Navigation from '../navigation/navigation';
+import Garage from '../garage/garage';
 import Winners from '../winners/winners';
-import createNewElement from "../create-new-element";
+import createNewElement from '../create-new-element';
 
 export default class Main {
-    body: HTMLBodyElement | null = document.querySelector('body');
-    container: HTMLDivElement | null = document.querySelector('.container');
-    navigation = new Navigation();
-    garage = new Garage();
-    winners = new Winners();
+    private navigation = new Navigation();
+    private garage = new Garage();
+    private winners = new Winners();
 
     async drawMain() {
-        const main = createNewElement<HTMLElement>('main', ['main']);
-
-        const navigation = this.navigation.drawNavigation();
-        const garage = await this.garage.drawGarage();
+        const main: HTMLElement = createNewElement('main', ['main']);
+        const navigation: HTMLDivElement = this.navigation.drawNavigation();
+        const garage: HTMLElement = await this.garage.drawGarage();
         const winners = await this.winners.drawWinners();
+
         main.append(navigation);
         main.append(garage);
         main.append(winners);
+
         return main;
     }
-
-    // drawMain() {
-    //     this.createMain();
-    //     this.navigation.drawNavigation();
-    //     this.garage.drawGarage();
-    //     this.winners.drawWinners();
-    // }
 }
