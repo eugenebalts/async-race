5bd3f2f4c5ba074e14af37306e786ce713b0955c
Yauheni Baltsevich <norcobeatz@gmail.com>
2023-07-30 01:28:28 +0200

    refactor: add types and split function in update-car.ts

--- async-race/src/components/view/update-car/update-car.ts
+++ async-race/src/components/view/update-car/update-car.ts
@@ -7,89 +7,132 @@ import Controller from '../../controller/controller';
 
 export default class UpdateCar {
     updated = false;
-    controller  = new Controller();
-    constructor() {
-
-    }
+    private controller  = new Controller();
+    private carIndex: number = -1;
+    private carColor: string = '#ffffff';
+    private carName: string = 'null';
+    private carToChangeName: HTMLHeadingElement | null = null;
+    private carToChangeWrapper: HTMLDivElement | null = null;
 
     drawUpdateWrapper(id: number) {
+        const updateWrapper: HTMLDivElement = createNewElement('div', ['garage__update-car']);
+        const updateContainer: HTMLDivElement = createNewElement('div', ['update-car__container']);
+        const carToChangeWrapper: HTMLDivElement = createNewElement('div', ['update-car__car-to-change']);
+        this.carToChangeWrapper = carToChangeWrapper;
+        const carToChangeImg: HTMLDivElement = this.getCarModel(id);
 
-        const updateWrapper = createNewElement('div', ['garage__update-car']);
+        const carToChangeName: HTMLHeadingElement = this.drawCarTitle(this.carName, id);
+        this.carToChangeName = carToChangeName;
+        const carEditorWrapper: HTMLDivElement = this.drawInputsWrapper(this.carName, this.carColor , id);
+        const closeButton = this.drawCloseButton();
 
-        const updateContainer = createNewElement('div', ['update-car__container']);
+        carToChangeWrapper.append(carToChangeName);
+        carToChangeWrapper.append(carToChangeImg);
+        updateContainer.append(closeButton);
+        updateContainer.append(carToChangeWrapper);
+        updateContainer.append(carEditorWrapper);
+        updateWrapper.append(updateContainer);
 
-        const carToChangeWrapper = createNewElement('div', ['update-car__car-to-change']);
-        let carsName = 'null';
-        let carsIndex = -1;
-        let carColor = '#ffffff';
-        const carToChangeImg = getCarModel();
+        return updateWrapper;
+    }
 
-        function getCarModel(newColor?: string) {
-            let color = 'null';
-            let name = 'null';
-            for (let i = 0; i < STATE.cars.length; i++) {
-                const car = STATE.cars[i];
-                if (car.id === id) {
-                    color = newColor ? newColor : car.color;
-                    carColor = car.color;
-                    name = car.name;
-                    carsName = car.name;
-                    carsIndex = i;
-                }
+    private drawCarTitle(carsName: string, id: number) {
+        const carToChangeName: HTMLHeadingElement = createNewElement('h3', ['track__title'], {textContent: `${carsName} №${id}`});
+
+        return carToChangeName;
+    }
+
+    private drawInputsWrapper(carsName: string, carColor: string, id: number) {
+        const carEditorWrapper: HTMLDivElement = createNewElement('div', ['form__inputs_update']);
+        const carEditorName: HTMLInputElement = this.drawCarEditorName(carsName, id);
+        const carEditorColor: HTMLInputElement = this.drawCarEditorColor(carColor, id);
+        const carEditorButton: HTMLButtonElement = this.drawCarEditorButton();
+
+        carEditorWrapper.append(carEditorName);
+        carEditorWrapper.append(carEditorColor);
+        carEditorWrapper.append(carEditorButton);
+
+        carEditorButton.addEventListener('click', () => {
+            if (carEditorName.value) {
+                STATE.cars[this.carIndex].color = carEditorColor.value;
+                STATE.cars[this.carIndex].name = carEditorName.value;
+                this.controller.updateCar(carEditorName.value, carEditorColor.value, id);
+                this.updated = true;
+            } else {
+                alert('Please, name a car!');
             }
-            return new Car(name, color, id).createCar();
-        }
+        });
 
-        const carToChangeName = createNewElement('h3', ['track__title'], {textContent: `${carsName} №${id}`});
-        // const carToChangeImg = new Car(carsModel.name, carsModel.color, carsModel.id).createCar();
+        return carEditorWrapper;
+    }
 
-        const inputsWrapper = createNewElement('div', ['form__inputs_update']);    
+    private drawCarEditorName(carsName: string, id: number) {
+        const carToChangeName = this.carToChangeName;
         const newCarInput: HTMLInputElement = createNewElement<HTMLInputElement>('input', ['form__input',
             'form__input_update', 'form__input_text'], {value: carsName, placeholder: 'Cars name'});
+
+        newCarInput.addEventListener('input', () => {
+            const value: string = newCarInput.value;
+            if (carToChangeName) {
+                carToChangeName.textContent = `${
+                    value.length > 10 ? value.slice(0, 10) + '... ' : value} №${id}`;
+            }
+        });
+
+        return newCarInput;
+    }
+
+    private drawCarEditorColor(carColor: string, id: number) {
+        const carToChangeWrapper = this.carToChangeWrapper;
         const newCarColor: HTMLInputElement = createNewElement<HTMLInputElement>('input', ['form__input',
             'form__input_update', 'form__input_color'], {type: 'color'});
         newCarColor.value = carColor;
-        const newCarButton = createNewElement('button', ['form__button', 'form__button_update'], {textContent: 'UPDATE'});
-
-        newCarInput.addEventListener('input', () => {
-            const value = newCarInput.value;
-            carToChangeName.textContent = `${
-                value.length > 10 ? value.slice(0, 10) + '... ' : value} №${id}`;
-        });
 
         newCarColor.addEventListener('input', () => {
-            if (carToChangeWrapper.children[1]) {
-                carToChangeWrapper.removeChild(carToChangeWrapper.children[1]);
-                carToChangeWrapper.append(getCarModel(newCarColor.value));
+            if (carToChangeWrapper) {
+                if (carToChangeWrapper.children[1]) {
+                    carToChangeWrapper.removeChild(carToChangeWrapper.children[1]);
+                    carToChangeWrapper.append(this.getCarModel(id, newCarColor.value));
+                }
             }
         });
 
-        newCarButton.addEventListener('click', () => {
-            // const car = STATE.cars[carsIndex];
+        return newCarColor;
+    }
 
-            STATE.cars[carsIndex].color = newCarColor.value;
-            STATE.cars[carsIndex].name = newCarInput.value;
+    private drawCarEditorButton() {
+        const newCarButton: HTMLButtonElement = createNewElement('button',
+            ['form__button', 'form__button_update'], {textContent: 'UPDATE'});
 
+        return newCarButton;
+    }
 
-            this.controller.updateCar(newCarInput.value, newCarColor.value, id);
-            this.updated = true;
-        });
-
+    private drawCloseButton() {
         const closeButton = createNewElement('div', ['update-car__close-btn']);
 
         closeButton.addEventListener('click', () => {
             this.updated = true;
         });
 
-        inputsWrapper.append(newCarInput);
-        inputsWrapper.append(newCarColor);
-        inputsWrapper.append(newCarButton);
-        carToChangeWrapper.append(carToChangeName);
-        carToChangeWrapper.append(carToChangeImg);
-        updateContainer.append(closeButton);
-        updateContainer.append(carToChangeWrapper);
-        updateContainer.append(inputsWrapper);
-        updateWrapper.append(updateContainer);
-        return updateWrapper;
+        return closeButton;
     }
+
+    private getCarModel(id: number, newColor? : string) {
+        let color: string = 'null';
+            let name: string = 'null';
+
+            for (let i = 0; i < STATE.cars.length; i++) {
+                const car = STATE.cars[i];
+                if (car.id === id) {
+                    color = newColor ? newColor : car.color;
+                    this.carColor = car.color;
+                    name = car.name;
+                    this.carName = car.name;
+                    this.carIndex = i;
+                }
+            }
+
+            return new Car(name, color, id).createCar();
+    }
+
 }
